0518752a9c41f870a17b02fb274260445ca6245e
Zak D Varty <44709288+zakvarty@users.noreply.github.io>
2022-11-08 10:48:55 +0000

    rename to "naming files" for more accurate title

---
+++ 01-02-naming-files/naming-things-outtakes.R
@@ -0,0 +1,55 @@
+----------
+  ## Summary
+
+
+  # Naming Things in Your Code
+
+  ```
+00-download.R
+01-wrangle.R
+02-model.R
+...
+09-model_evaluation.R
+10-model_comparison_plots.R
+```
+
+- Where to split is an art.
+
+- Aim for files that can be named concisely while still evoking thier purpose.
+
+- All `library()` calls go at the top of a script.
+
+- Use commented lines of `-` and `=` to break the file into readable chunks
+
+```
+# Load data -----------------------
+
+# Plot data -----------------------
+```
+
+## Syntax - object names
+
+- Name varaibles and functions using only lowecase letters, numbers and `_`
+
+- Do not used dots in function names (looking at you, python people). These should be reseved for methods in the S3 OOP (`funcion.class`).
+
+- Names are not the place for data: if you have model_2018 , model_2019 and  model_2020 consider storing these as a list or data.frame called `models`.
+
+- variable names $=$ nouns, function names $=$ verbs.
+
+- Where possible, do not rename common function and variables
+
+```{r}
+T <- FALSE
+c <- 10
+mean <- function(x) sum(x)
+```
+
+## Syntax - Spacing
+
+- Spaces go after all commas, never before. Just like English.
+
+-
+
+  ## References {visibility="uncounted"}
+
